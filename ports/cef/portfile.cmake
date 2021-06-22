e765b9501c0d620b0dc8ae06a6b26f42108dd8b4
Serguei <serkhas@hotmail.com>
2021-06-22 17:58:31 -0400

    fix cef port

--- ports/cef/portfile.cmake
+++ ports/cef/portfile.cmake
@@ -1,9 +1,7 @@
 vcpkg_from_github(
 	OUT_SOURCE_PATH SOURCE_PATH
-	REPO northwindtraders/beicode
-	REF 19a1f95c2f56a27ced90227b5e2754a602a08e69
-	SHA512
-7b2bb7acb2a8ff07bff59cfa27247a7b2cced03828919cd65cc0c8cf1f724f5f1e947ed6992dcdbc913fb470694a52613d1861eaaadbf8903e94eb9cdfe4d000
+	REPO serg06/vcpkg-cef-test-priv
+	REF 17ba063fce2e166002a17fd62fbcd9bab1ff828e
 	HEAD_REF main
 )
 
@@ -14,9 +12,9 @@ vcpkg_configure_cmake(
 vcpkg_install_cmake()
 vcpkg_fixup_cmake_targets()
 
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+# file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
-file(
-	INSTALL "${SOURCE_PATH}/LICENSE"
-	DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
-	RENAME copyright)
+# file(
+# 	INSTALL "${SOURCE_PATH}/LICENSE"
+# 	DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
+# 	RENAME copyright)
