691e9e8cbd31ec8e3fb56d43886a9dbcb31c6689
Serguei <serkhas@hotmail.com>
2021-06-22 18:06:07 -0400

    add sha512sum

--- ports/cef/portfile.cmake
+++ ports/cef/portfile.cmake
@@ -2,6 +2,7 @@ vcpkg_from_github(
 	OUT_SOURCE_PATH SOURCE_PATH
 	REPO serg06/vcpkg-cef-test-priv
 	REF 17ba063fce2e166002a17fd62fbcd9bab1ff828e
+	SHA512 cf83e1357eefb8bdf1542850d66d8007d620e4050b5715dc83f4a921d36ce9ce47d0d13c5d85f2b0ff8318d2877eec2f63b931bd47417a81a538327af927da3e
 	HEAD_REF main
 )
 
